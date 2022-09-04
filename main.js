7d1223526ec98756278ba471dde6e0590fce8a3b
Matthew Peterson <matthew.peterson.39@hotmail.com>
2022-09-04 13:44:49 +0300

    Enabled click to draw and show grid function

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const DEFAULT_COLOR = 'black';
 const DEFAULT_SIZE = 16;
+var rainbowSelected = false;
+var enableDraw = false;
+var showGrid = true;
 
 const rgbToHex = (r, g, b) => '#' + [r, g, b].map(x => {
     const hex = x.toString(16)
     return hex.length === 1 ? '0' + hex : hex
-  }).join('')
+  }).join('');
 
 var current_size = DEFAULT_SIZE;
 var current_color = DEFAULT_COLOR;
@@ -19,13 +22,32 @@ function draw_grid(size) {
     for (let i = 0; i < (size * size); i++) {
         let cell = document.createElement("div");
         cell.id = 'grid-cell';
-        cell.style.backgroundColor = 'peru';
-        cell.addEventListener('mouseover', () => cell.style.backgroundColor = `${current_color}`);
+        cell.style.cursor = 'pointer';
+        cell.style.backgroundColor = 'white';
+        if(showGrid) {
+            cell.style.borderStyle = 'solid';
+            cell.style.borderColor = 'black';
+            cell.style.borderWidth = '.25px';
+        }
+        cell.addEventListener('click', () => {
+            if(enableDraw) { 
+                enableDraw = false;
+            }
+            else {
+                enableDraw = true;
+            }
+    });
+        cell.addEventListener('mouseover', () => {
+            if(enableDraw) {
+                cell.style.backgroundColor = `${current_color}`
+            }});
+
         grid_container.insertAdjacentElement('beforeend', cell);
     };
 };
 
-draw_grid(16);
+//Draw initial grid at load time.
+draw_grid(DEFAULT_SIZE);
 
 function change_size(input) {
     draw_grid(Math.abs(input));
@@ -45,8 +67,18 @@ function get_current_size() {
     return current_size;
 }
 
+function rainbow_select(new_value) {
+    rainbowSelected = new_value;
+    return;
+}
+
 function set_color(new_color) {
-    current_color = new_color;
+    if(rainbowSelected) {
+        current_color = random();
+    }
+    else {    
+        current_color = new_color;
+    }
     document.getElementById('color-picker').value = `${current_color}`;
 }
 
@@ -54,6 +86,16 @@ function random() {
     let r = Math.random() * (256 - 0) + 0;
     let g = Math.random() * (256 - 0) + 0;
     let b = Math.random() * (256 - 0) + 0;
-    let hex = rgbToHex(r,g,b);
-    return hex;
 }
+
+function hide_grid_lines() {
+    if(showGrid) {
+        showGrid = false;
+    }
+    else {
+        showGrid = true;
+    }
+    draw_grid(current_size);
+
+}
+
